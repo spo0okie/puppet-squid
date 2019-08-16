5ed08c87279a63973bfe4ec995734d1ec277b005
spo0okie <reviakin@gmail.com>
2019-08-16 23:01:31 +0500

    добавлена возможность менять имя интерфейса

--- manifests/iptables.pp
+++ manifests/iptables.pp
@@ -1,10 +1,13 @@
-define squid::iptables {
+define squid::iptables(
+	$if	= 'eth0',		#по умолчанию интерфейс eth0
+) {
 	file {'/etc/init.d/squid.iptables':
 		ensure		=> file,
 		mode		=> '0777',
 		content		=> "#!/bin/sh
-		/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to $title:3129
-		/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j DNAT --to $title:3130
+		/sbin/iptables -t nat -A PREROUTING -i $if -p tcp --dport 80 -j DNAT --to $title:3129
+		/sbin/iptables -t nat -A PREROUTING -i $if
+		-p tcp --dport 443 -j DNAT --to $title:3130
 		",
 	} ->
 	exec {'run DNAT for Squid':
