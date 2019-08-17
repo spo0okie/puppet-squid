cca7b6513b13e261d9a621d84d849c90128365ad
spo0okie <reviakin@gmail.com>
2019-08-17 11:58:29 +0500

    добавлен пее

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,18 +8,21 @@ class squid {
 		mode	=> '0640',
 		owner	=> 'squid',
 		source	=> 'puppet:///modules/squid/squid.conf',
+		notify	=> Service['squid'],
 	} ->
 	file {'/etc/squid/squidCA.pem':
 		ensure	=> file,
 		mode	=> '0640',
 		owner	=> 'squid',
 		source	=> 'puppet:///modules/squid/squidCA.pem',
+		notify	=> Service['squid'],
 	} ->
 	file {'/etc/squid/url-list':
 		ensure	=> directory,
 		mode	=> '0755',
 		source	=> 'puppet:///modules/squid/url-list',
 		recurse	=> true
+		notify	=> Service['squid'],
 	} ->
 	exec {'init squid cert storage': 
 		command => '/usr/lib64/squid/security_file_certgen -c -s /var/lib/ssl_db -M 4MB',
